1dbc61a1f4b61a98e2c23b7bbed15ee99f4809c9
Kamryn <kamrynrobb07@gmail.com>
2026-07-16 07:34:23 +0100

    Minor misc changes

--- Coursework/CMP105App/Player.h
+++ Coursework/CMP105App/Player.h
@@ -49,11 +49,11 @@ private:
     const float SPRINT_SPEED_MULT = 1.10f;
     const float SPEED = 2.25f;
     const float GRAVITY = 15.0f;
-    const float COEFF_OF_REST = 1.0f;                   // tweak all this for extremely snappy on-ground movement
+    const float COEFF_OF_REST = 1.0f;                   // tweak all this for extremely snappy movement (aka get rid of acceleration and tweak other random stuff)
     const float DRAG_FACTOR = 0.0f;
     const float AIR_DRAG_FACTOR = 0.99f;
     const float TURN_DRAG = 1.0f;
-    const float JUMP_FORCE = 6.5f;
+    const float JUMP_FORCE = 4.5f;
     const float SPRINT_ANIM_THRESHOLD = 999.0f;     //1.2f * SPEED;     // set sprint anim threshold real high for now until (if) sprint frames are animated for new spritesheet
     const float ACTIVATE_RANGE_SQUARED = 700.0f;
 
